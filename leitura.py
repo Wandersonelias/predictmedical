5248dfa40d8aadffa1b64d149994b96d8e00d8f4
unknown <wandersonelias55@gmail.com>
2026-04-11 10:24:09 -0300

    configurações de projeto

--- leitura.py
+++ leitura.py
@@ -1,1 +1,16 @@
-print("Wanderson")
+from services.datasetService import dataset_completo
+
+#Exibir data 
+
+
+
+def imprimir_dados():
+    print("Dados")
+    df = dataset_completo()
+    return df
+
+
+imprimir_dados()
+
+if __name__ == "__main__":
+    print("Wanderson Elias")
